32da7fe38fe0451bcc22199ff02a5aac74d90c8f
yoguoran <79782464+yoguoran@users.noreply.github.com>
2025-08-09 22:53:21 +0800

    Update htmlBuilder.js

--- src/htmlBuilder.js
+++ src/htmlBuilder.js
@@ -21,7 +21,7 @@ const generateHead = () => `
     <meta property="og:title" content="">
     <meta property="og:description" content="强大的在线订阅链接转换工具,支持多种代理协议和自定义规则">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://sublink-worker.sageer.me/">
+    <meta property="og:url" content="">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/qrcode-generator@1.4.4/qrcode.min.js"></script>
